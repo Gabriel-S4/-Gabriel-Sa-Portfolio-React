18f5a01edd3705b01cb8760ce84e26900a42b410
Gabriel-S4 <gabsa2300@gmail.com>
2026-01-26 01:09:53 -0300

    fix home folder

---
+++ src/pages/home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Hero from '../components/hero'
+import PurpleTransition from '../components/PurpleTransition'
+import AboutMe from '../components/about-me'
+import Projects from '../components/projects'
+import Footer from '../components/footer'
+import Hr from '../components/hr'
+
+function home() {
+  return (
+    <>
+        <Hero />
+        <PurpleTransition />
+        <Hr />
+        <AboutMe />
+        <Hr />
+        <Projects />
+        <Hr />
+        <Footer />
+    </>
+  )
+}
+
+export default home
